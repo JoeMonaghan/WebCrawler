d582395d151195822056898ecc1d024036553028
Joseph Monaghan <j.j.monaghansoftware@gmail.com>
2018-06-02 10:41:03 +0100

    requests and beautiful soup imported

--- Main.py
+++ Main.py
@@ -1,12 +1,26 @@
-
-
-
-
-
+import requests
+import timeit
+from bs4 import BeautifulSoup
 
 
 def main():
     print('working')
+    response = requests.get('https://www.bloomberg.com/europe')
+    print(response.status_code)
+    page = response.text
+    print(type(page))
+
+    soup = BeautifulSoup(page, 'html.parser')
+
+    print(soup.title)
+    print(soup.find_all('a'))
+
+    print('*\n' *12)
+    print(soup.find(id="bb-lazy-img-328028706"))
+    #print(soup.prettify())
+
+
+
 
 
 if __name__ == '__main__':
