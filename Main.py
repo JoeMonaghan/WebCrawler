58866fe29f2c38a27fde52a827ff9fce0a6cf5a4
Joseph Monaghan <j.j.monaghansoftware@gmail.com>
2018-06-06 19:06:27 +0100

    Add a program overview to the start of Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,23 @@
 import requests
-import timeit
 from bs4 import BeautifulSoup
+
+'''
+Given a starting url of ant wikipedia page, work if it possible to get to https://en.wikipedia.org/wiki/Philosophy
+by only clicking on the first link of each article. Ensure not to get stuck in loop i.e visiting the same
+page twice , which should return false. Give a max number of visited pages to 25 if not specified by user.
+Ensure not to over load server with lots of requests.
+'''
+
+
+
+def continue_crawl(search_history, target_url):
+    '''
+
+    :param search_history:
+    :param target_url:
+    :return:
+    '''
+    pass
 
 
 def main():
